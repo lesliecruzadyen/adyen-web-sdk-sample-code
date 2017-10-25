b95fa03be0e3d599c5417ebc56cacb94009ca08a
lesliecruzadyen <32970195+lesliecruzadyen@users.noreply.github.com>
2017-10-25 10:59:48 -0700

    Update order.php

--- payment/order.php
+++ payment/order.php
@@ -27,7 +27,7 @@
 $value = 120;
 
 /** @var  $currencyCode - Change this to any currency you support: https://docs.adyen.com/developers/currency-codes */
-$currencyCode = 'USD';
+$currencyCode = 'EUR';
 
 /** @array $amount - Amount is a combination of value and currency */
 $amount = [
@@ -42,10 +42,10 @@ $reference = 'order_id';
 $shopperReference = 'example_shopper';
 
 /** @var $shopperLocale - The shopper locale */
-$shopperLocale = 'en-US';
+$shopperLocale = 'nl_NL';
 
 /** @var $countryCode - The countrycode influences the returned payment methods */
-$countryCode = 'US';
+$countryCode = 'NL';
 
 /** @var $channel - the channel influences the returned payment methods (the same server can be used for iOS, Android and Point of sale */
 $channel = 'Web';
